97590b420da20ebc70dd4a779849931cffa62261
Oana Berentan <oana.berentan@yahoo.com>
2019-09-21 18:09:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,4 +8,11 @@ INSERT INTO books (title, year, series_id) VALUES("A clash of kings", 2000, 2);
 INSERT INTO books (title, year, series_id) VALUES("A storm of swords", 2002, 2);
 INSERT INTO books (title, year, series_id) VALUES("A dance with a dragon", 2008, 2);
 
-INSERT INTO characters (name, species, motto, author_id) VALUES
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Daeneryes", "dragon", "Queen of Dragons", 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("Jon Snow", "human", "Winter is coming", 2);
+INSERT INTO characters (name, species, motto, author_id) VALUES ("");
+INSERT INTO characters (name, species, motto, author_id) VALUES ();
+INSERT INTO characters (name, species, motto, author_id) VALUES ();
+INSERT INTO characters (name, species, motto, author_id) VALUES ();
+INSERT INTO characters (name, species, motto, author_id) VALUES ();
+INSERT INTO characters (name, species, motto, author_id) VALUES ();
