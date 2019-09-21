53b1768b5c63813affabc81e944903f627e66901
Oana Berentan <oana.berentan@yahoo.com>
2019-09-21 18:58:10 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,8 +23,11 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenre.name
-   FROM authors
-   JOIN subgenre"
+   FROM series
+   INNER JOIN authors
+   ON series.author_id = authors.id
+   INNER JOIN subgenres
+   ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
