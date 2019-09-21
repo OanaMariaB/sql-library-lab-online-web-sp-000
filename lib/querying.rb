13821d7e5aa47e991f9d291e4f26a121ca396e32
Oana Berentan <oana.berentan@yahoo.com>
2019-09-21 18:50:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT COUNT(species) FROM characters"
+  "SELECT COUNT(species)
+   FROM characters
+   GROUP BY species
+   "
 end
 
 def select_name_and_series_subgenres_of_authors
