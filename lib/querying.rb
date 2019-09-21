112a819f2cd048f42da6ed72720fc200046133ef
Oana Berentan <oana.berentan@yahoo.com>
2019-09-21 18:51:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
    FROM characters
    GROUP BY species
-   ORFER BY COUNT(species)
+   ORdER BY COUNT(species)
    DESC LIMIT 1
    "
 end
