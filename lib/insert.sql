bb8e0fa79c57d2212e57134fb29d0107dd935f95
Oana Berentan <oana.berentan@yahoo.com>
2019-09-21 18:04:04 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES("Lord of the Rings", 1, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES("Game of Thrones", 2, 1);
+
+INSERT INTO books (title, year, series_id) VALUES
+("The fellowship of the ring", 1994, 1),
+("The return of the king", 1998, 1),
+("The hobbit", 2002, 1),
+("A clash of kings", 2000, 2),
+("A storm of swords", 2002, 2),
+("A dance with a dragon", 2008, 2);
+
+INSERT INTO CHARACTERS
